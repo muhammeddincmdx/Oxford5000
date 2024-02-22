cafd150da9a977e976a2dac9661150181bc67bde
Muhammed Dinç <54439858+muhammeddincmdx@users.noreply.github.com>
2024-02-22 13:39:57 +0300

    Update translate.py
    adding voice feature, changing word selecting algorithm, showing correct and false answer.

--- translate.py
+++ translate.py
@@ -1,6 +1,8 @@
 import random
 from colorama import Fore, Back, Style
 from googletrans import Translator
+import pyttsx3
+
 translate = Translator()
 w=[]
 redBack=Back.RED
@@ -23,25 +25,48 @@ with open("words.txt", "r") as f:
                 l.append(i)
         w.append(l)
 
-        
+used_words = []
+wrong_words = []
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
 if __name__=="__main__":
     print(Fore.MAGENTA+
             """\n\tIMPROVE YOUR ENGLISH\n"""+reset)
 
     while True:
-        print(cyan+"press q to quit"+reset)
+        print(cyan+"press 'q' to quit or 's' to show results"+reset)
         wr=random.choice(w)
         word=wr[0].lower()
         meaning=translate.translate(word, dest='tr').text.lower().capitalize()
-       
-        print(f"Word: {blue} {word.capitalize()} {reset} Type: {yellow} {wr[1].capitalize()} {reset} Level: {green} {wr[2]} {reset}")
+
+        while word in used_words:
+            wr=random.choice(w)
+            word=wr[0].lower()
+            meaning=translate.translate(word, dest='tr').text.lower().capitalize()
+
+        print(f"Word:{blue} {word.capitalize()} {reset} Type: {yellow} {wr[1].capitalize()} {reset} Level: {green} {wr[2]} {reset}")
+        speak(f"{word.capitalize()}")  # Kelimeyi seslendir
         k=input("Enter the meaning of the word: ").capitalize()
+
         if k=="q" or k=="Q":
-            break
+            print(f"\n{greenBack}Correct Answers:{reset} {', '.join([green + word.capitalize() + reset if word in used_words else cyan + word.capitalize() + reset for word in used_words])}")
+            print(f"{redBack}Wrong Answers:{reset} {', '.join([red + word.capitalize() + reset if word in wrong_words else cyan + word.capitalize() + reset for word in wrong_words])}\n")
+            m = input("Press 'm' to continue or any other key to quit: ")
+            if m.lower() == 'm':
+                continue
+            else:
+                break
+        elif k=="s" or k=="S":
+            print(f"\n{greenBack}Correct Answers:{reset} {', '.join([green + word.capitalize() + reset if word in used_words else cyan + word.capitalize() + reset for word in used_words])}")
+            print(f"{redBack}Wrong Answers:{reset} {', '.join([red + word.capitalize() + reset if word in wrong_words else cyan + word.capitalize() + reset for word in wrong_words])}\n")
+            continue
         elif k==meaning:
+            used_words.append(word)
             print(f"{greenBack}CORRECT!!{reset}\n")
-
         elif k!=meaning:
+            wrong_words.append(word)
             print(f"{redBack}WRONG!!{reset} The meaning of the word is {cyan} {meaning} {reset}\n")
-            
-
